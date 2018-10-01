3a38f05aed3ada2043ca7263f2bff27a1c84a175
Chen Tan <admin8978boy@gmail.com>
2018-10-01 15:20:08 +0900

    feat: adding a e2e test

---
+++ test-e2e/HomePage.js
@@ -0,0 +1,7 @@
+import { Selector } from 'testcafe';
+
+// NOTE: the app will be saved from /dist with http-server, which defaults at 8080 port
+fixture('HomePage').page('http://localhost:8090');
+test('Test Anki title page', async (t) => {
+  await t.expect(Selector('#title').innerText).eql('Anki');
+});

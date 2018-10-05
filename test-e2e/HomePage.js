d16d6c84068e356a8f78d8220e340b3b815b694b
Chen Tan <admin8978boy@gmail.com>
2018-10-05 16:45:20 +0900

    wip

--- test-e2e/HomePage.js
+++ test-e2e/HomePage.js
@@ -1,7 +1,7 @@
 import { Selector } from 'testcafe';
 
 // NOTE: the app will be saved from /dist with http-server, which defaults at 8080 port
-fixture('HomePage').page('http://localhost:8090'); // docker.for.mac.localhost works on local
+fixture('HomePage').page('http://10.0.0.10'); // docker.for.mac.localhost works on local mac when accessing from docker, --network=host is not needed
 test('Test Anki title page', async (t) => {
   await t.expect(Selector('#title').innerText).eql('Anki');
 });

5ac0577464c60630c4fafab63c2a2c2df1f98280
Chen Tan <admin8978boy@gmail.com>
2018-10-11 14:40:22 +0900

    fix: e2e test

--- test-e2e/HomePage.js
+++ test-e2e/HomePage.js
@@ -1,7 +1,10 @@
 import { Selector } from 'testcafe';
 
 // NOTE: the app will be saved from /dist with http-server, which defaults at 8080 port
-fixture('HomePage').page('http://10.0.0.10'); // docker.for.mac.localhost works on local mac when accessing from docker, --network=host is not needed
+
+const url = process.env.NODE_ENV === 'production' ? 'http://10.0.0.10' : 'http://localhost:8090';
+
+fixture('HomePage').page(url); // docker.for.mac.localhost works on local mac when accessing from docker, --network=host is not needed
 test('Test Anki title page', async (t) => {
-  await t.expect(Selector('#title').innerText).eql('Anki');
+  await t.expect(Selector('h2').innerText).eql('Log In');
 });
